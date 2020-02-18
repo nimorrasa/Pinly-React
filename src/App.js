2086e186cf00f8a02f82671af4042ba225a32c1a
Orrasa Puangkaew <orrasa.nim@gmail.com>
2020-02-18 18:02:05 +0700

    add rsu font

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './fonts/rsufont/RSU_Regular.ttf'
+import './fonts/rsufont/RSU_light.ttf'
+import './fonts/rsufont/RSU_BOLD.ttf'
 import './css/App.css';
 import { Home } from './pages/Home.js';
 
