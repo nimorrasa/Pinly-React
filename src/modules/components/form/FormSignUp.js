0046dc1b52e166bb5c4c484f111e71039fe8a7a0
Orrasa Puangkaew <orrasa.nim@gmail.com>
2020-02-27 01:24:33 +0700

    layout signup

--- src/modules/components/form/FormSignUp.js
+++ src/modules/components/form/FormSignUp.js
@@ -10,14 +10,30 @@ const FormSignUp = (props) => {
 
   
 
-  const nextStep = useCallback(() => {
-    setStep('sign_up_step_2');
-  },[setStep]);
+  const nextStep = useCallback(() => { setStep('sign_up_step_2'); },[setStep]);
+  const backStep = useCallback(() => { setStep('sign_up_step_1'); },[setStep]);
 
 
   const onSubmit = values => {
     console.log(values);
   };
+
+
+  const dates = []
+
+  for (let i=1; i<=31; i++) {
+    dates.push(i)
+  }
+
+  const months = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+
+  const years = []
+
+  for (let i=2020; i>=1999; i--) {
+    years.push(i)
+  }
+
+  console.log(dates);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -45,7 +61,7 @@ const FormSignUp = (props) => {
             </Col>
           </Row>
           <Row>
-            <Col id="password" xs='6'>
+            <Col id="password" lg='4' xs='6'>
             <p class="m-0">Password</p>
             <input
               type="password"
@@ -54,7 +70,7 @@ const FormSignUp = (props) => {
               placeholder="password"
               required/>
             </Col>
-            <Col id="confirm_password" xs='6'>
+            <Col id="confirm_password" lg='4' xs='6'>
             <p class="m-0">Confirm Password</p>
             <input
               type="password"
@@ -63,22 +79,48 @@ const FormSignUp = (props) => {
               required/>
             </Col>
           </Row>
+          <Row> 
+            <Col id="bdate" lg='2' xs='2'>
+              <p class="m-0">Birthdate</p>
+              <select value='1' name="bdate" min='12' max='31'required>
+                {dates.map((value, index) => {
+                  return <option key={value}>{value}</option>;
+                })}
+              </select>
+            </Col>
+            <Col id="mdate" lg='3' xs='4'>
+              <p class="m-0" style={{color: "transparent"}}>Birthdate</p>
+              <select value='1' name="bmonth" min='12' max='31'required>
+                {months.map((value, index) => {
+                  return <option key={index+1}>{value}</option>;
+                })}
+              </select>
+            </Col>
+            <Col id="ydate" lg='3' xs='4'>
+              <p class="m-0" style={{color: "transparent"}}>Birthdate</p>
+              <select value='1' name="byear" min='12' max='31'required>
+                {years.map((value, index) => {
+                  return <option key={value}>{value}</option>;
+                })}
+              </select>
+              </Col>
+          </Row>
           <Row>
-            <Col className="button" xs='12'>
+            <Col className="button" lg='3' xs='3'>
               <button type="button" onClick={nextStep}>Continue</button>
             </Col>
           </Row>
           </Col>
           <Col className='sign_up_2' xs='12'>
-          <Row id="gender"> 
-            <Col xs='4'>
+          <Row> 
+            <Col id="gender" lg='2' xs='3'>
               <p class="m-0">Gender</p>
               <select name="gender" required>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
               </select>
             </Col>
-            <Col xs='4'>
+            <Col id="weight" lg='3' xs='4'>
               <p class="m-0">Weight</p>
               <input
                 type="number"
@@ -86,7 +128,7 @@ const FormSignUp = (props) => {
                 placeholder="weight"
                 required/>
             </Col>
-            <Col id="height" xs='4'>
+            <Col id="height" lg='3' xs='4'>
             <p class="m-0">Height</p>
             <input
               type="number"
@@ -97,7 +139,7 @@ const FormSignUp = (props) => {
             </Col>
           </Row>
           <Row>
-            <Col id="disease" xs='6'>
+            <Col id="disease" xs='4'>
             <p class="m-0">Congenital disease</p>
             <input
               type="disease"
@@ -107,7 +149,10 @@ const FormSignUp = (props) => {
             </Col>
           </Row>
           <Row>
-            <Col className="button" xs='12'>
+            <Col className="button" lg='3' xs='3'>
+              <button type="submit" onClick={backStep}>Back</button>
+            </Col>
+            <Col className="button" lg='3' xs='3'>
               <button type="submit" onClick={onSubmit}>Submit</button>
             </Col>
           </Row>
