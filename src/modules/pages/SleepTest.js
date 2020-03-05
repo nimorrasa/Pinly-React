bbd8852882fbb056ae31b0e185d3428132f7da6e
Orrasa Puangkaew <orrasa.nim@gmail.com>
2020-03-06 00:03:06 +0700

    done sleep test

--- src/modules/pages/SleepTest.js
+++ src/modules/pages/SleepTest.js
@@ -1,5 +1,9 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import MyNavbar from '../components/navbar/MyNavbar.js';
+import { Row, Col, CardBody, Card, CardTitle, CardText, Button, Media } from "reactstrap";
+import '../css/SleepTest.css';
+import sleep_score from '../../images/button/sleep_score.png';
+import go_to_sleep from '../../images/button/go_to_sleep.png';
 
 const SleepTest = (props) => {
 
@@ -18,7 +22,24 @@ const SleepTest = (props) => {
         <div>
             <MyNavbar theme={navbarTheme} onChangeTheme={handleNavbarThemeChange} hideThemeSwitch={false}></MyNavbar>
             <div className={"App Sleep_test "+theme}>
-                Sleep Test
+                <Row>
+                    <Col lg="6" md="6" xs="12">
+                        <Card>
+                            <CardTitle>Show sleep score</CardTitle>
+                            <CardBody>
+                                <a href="https://www.google.com"><img className="button"  src={sleep_score}></img></a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col lg="6" md="6" xs="12">
+                        <Card>
+                            <CardTitle>Ready to Sleep?</CardTitle>
+                            <CardBody>
+                                <a href="https://www.facebook.com"><img className="button"  src={go_to_sleep}></img></a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
