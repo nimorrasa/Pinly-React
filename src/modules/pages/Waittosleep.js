c875e5895a7e2bcdd9e9bdd1118f2cd6a1255d53
Hasaneeya Kaenthram <33242409+Nyaqxii@users.noreply.github.com>
2020-03-09 16:42:42 +0700

    Update Waittosleep.js

--- src/modules/pages/Waittosleep.js
+++ src/modules/pages/Waittosleep.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import './components/text.css';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState, useCallback, useEffect } from 'react';
+import MyNavbar from '../components/navbar/MyNavbar.js';
+import { Row, Col, CardBody, Card, CardSubtitle, CardTitle, CardText, Button } from "reactstrap";
+
 import IconWaittosleep from './components/WaittoSleepPage/ic_waitsleep.png'
+import './components/text.css';
 import './App.css';
 
 var colorheader ={
@@ -21,6 +23,18 @@ var colorfont ={
   fontSize: 30
 }
 const Waittosleep = (props) => {
+  
+  const [theme,setTheme] = useState(props.theme);
+    const [navbarTheme, setNavbarTheme] = useState(props.theme === 'theme_dark' ? 'dark' : 'light');
+
+    const handleNavbarThemeChange = useCallback((current_theme) => {
+      setNavbarTheme(current_theme);
+      setTheme('theme_'+current_theme);
+      props.onChangeTheme('theme_'+current_theme);
+    },[setNavbarTheme,setTheme]);
+  
+    useEffect(() => { setTheme(props.theme)});
+  
   return (
     <div className="Bg-color">
     
