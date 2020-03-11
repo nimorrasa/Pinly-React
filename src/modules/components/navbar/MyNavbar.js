e899c0a7bac048fc970debbccee4d69ab3e59a4b
Orrasa Puangkaew <orrasa.nim@gmail.com>
2020-03-11 19:14:18 +0700

    edit navbar

--- src/modules/components/navbar/MyNavbar.js
+++ src/modules/components/navbar/MyNavbar.js
@@ -1,76 +1,96 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
-  BrowserRouter as Router,
-  Link,
+	BrowserRouter as Router,
+	Link,
 } from 'react-router-dom';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Media
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	Media
 } from 'reactstrap';
 import ThemeSwitch from '../toggle_switch/ThemeSwitch.js';
 import MyDropdown from '../button/MyDropdown.js';
 import MySetting from '../setting/MySetting.js';
 import MyButton from '../button/MyButton.js';
 import logo from '../../../images/logo.png';
-import { withCookies, Cookies } from 'react-cookie';
+import firebase from 'firebase';
 
 
 const MyNavbar = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDark, setIsDark] = useState((props.theme === 'dark'));
-  const [theme, setTheme] = useState(props.theme);
+	const [isOpen, setIsOpen] = useState(false);
+	const [isDark, setIsDark] = useState((props.theme === 'dark'));
+	const [theme, setTheme] = useState(props.theme);
 
-  const toggle = () => setIsOpen(!isOpen);
+	const toggle = () => setIsOpen(!isOpen);
 
-  const handleThemeChange = useCallback((new_theme) => {
-    const current_theme = new_theme ? 'dark' : 'light';
-    setIsDark(new_theme);
-    setTheme(current_theme);
-    props.onChangeTheme(current_theme);
-  },[setIsDark,setTheme]);
+	const [isAuth,setIsAuth] = useState(false);
 
-  return (
-    <div>
-      <Navbar bg={theme} light={theme === 'light'} dark={theme === 'dark'}  expand="md">
-        <NavbarBrand href="/">
-            <Media className="App-header-logo"  src={logo}></Media>
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <Link to='/home'><MyButton type="text" title="Home"></MyButton></Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/profile'><MyButton type="text" title="Profile"></MyButton></Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/sleep_test'><MyButton type="text" title="Sleep Test"></MyButton></Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/contact_us'><MyButton type="text" title="Contact"></MyButton></Link>
-            </NavItem>
-            <NavItem>
-              <MyDropdown theme={theme} alignment="left" titleType="icon" title="fa fa-question-circle" items={["How to Registeration","How to Sleeptest",'Assistant']}></MyDropdown>
-            </NavItem>
-          </Nav>
-          <Nav className="mr-auto-right" navbar>
-            <NavItem style={{display: (props.hideThemeSwitch ? 'none' : 'block')}}>
-              <ThemeSwitch isDark={isDark} onChangeTheme={handleThemeChange}></ThemeSwitch>
-            </NavItem>
-            <NavItem>
-              <MySetting theme={theme}></MySetting>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+	const handleThemeChange = useCallback((new_theme) => {
+		const current_theme = new_theme ? 'dark' : 'light';
+		setIsDark(new_theme);
+		setTheme(current_theme);
+		props.onChangeTheme(current_theme);
+	},[setIsDark,setTheme]);
+
+	useEffect(() => {
+		async function fetchData (user_id) {
+				let user = await firebase.database().ref('/users/' + user_id).once('value');
+				return user.val();
+			}
+		
+			firebase.auth().onAuthStateChanged(async function(user) {
+				if (user) {
+						let data = await fetchData(user.uid);
+						setIsAuth(true);
+					}else{
+						setIsAuth(false)
+					}
+				});
+			
+
+			},[]);
+
+	return (
+		<div>
+		<Navbar bg={theme} light={theme === 'light'} dark={theme === 'dark'}  expand="md">
+			<NavbarBrand href="/">
+				<Media className="App-header-logo"  src={logo}></Media>
+			</NavbarBrand>
+			<NavbarToggler onClick={toggle} />
+			<Collapse isOpen={isOpen} navbar>
+			<Nav className="mr-auto" navbar>
+				<NavItem>
+				<Link to='/home'><MyButton type="text" title="Home"></MyButton></Link>
+				</NavItem>
+				<NavItem style={{display : (isAuth ? 'block' : 'none')}}>
+				<Link to='/profile'><MyButton type="text" title="Profile"></MyButton></Link>
+				</NavItem>
+				<NavItem style={{display : (isAuth ? 'block' : 'none')}}>
+				<Link to='/sleep_test'><MyButton type="text" title="Sleep Test"></MyButton></Link>
+				</NavItem>
+				<NavItem>
+				<Link to='/contact_us'><MyButton type="text" title="Contact"></MyButton></Link>
+				</NavItem>
+				<NavItem>
+				<MyDropdown theme={theme} alignment="left" titleType="icon" title="fa fa-question-circle" items={(isAuth ? ["How to Registeration","How to Sleeptest","Assistant"] : ["How to Registeration","How to Sleeptest"])}></MyDropdown>
+				</NavItem>
+			</Nav>
+			<Nav className="mr-auto-right" navbar>
+				<NavItem style={{display: (props.hideThemeSwitch ? 'none' : 'block')}}>
+				<ThemeSwitch isDark={isDark} onChangeTheme={handleThemeChange}></ThemeSwitch>
+				</NavItem>
+				<NavItem>
+				<MySetting theme={theme}></MySetting>
+				</NavItem>
+			</Nav>
+			</Collapse>
+		</Navbar>
+		</div>
+	);
 }
 
 export default MyNavbar;
--- src/modules/pages/asst.js
+++ src/modules/pages/asst.js
@@ -1,8 +1,11 @@
 import React, { useState, useCallback, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 import MyNavbar from '../components/navbar/MyNavbar.js';
+import firebase from 'firebase';
 
 const Assistant = (props) => {
-
+    const history = useHistory();
+    const [isLoading,setIsLoading] = useState(true);
     const [theme,setTheme] = useState(props.theme);
     const [navbarTheme, setNavbarTheme] = useState(props.theme === 'theme_dark' ? 'dark' : 'light');
 
@@ -14,9 +17,32 @@ const Assistant = (props) => {
     
     useEffect(() => { setTheme(props.theme)});
 
+
+  useEffect(() => {
+    async function fetchData (user_id) {
+        let user = await firebase.database().ref('/users/' + user_id).once('value');
+        return user.val();
+    }
+
+    firebase.auth().onAuthStateChanged(async function(user) {
+        if (user) {
+            let data = await fetchData(user.uid);
+        }else{
+            history.push('/login');
+        }
+        setIsLoading(false);
+    });
+
+
+},[]);
+
+
     return (
         <div>
             <MyNavbar theme={navbarTheme} onChangeTheme={handleNavbarThemeChange} hideThemeSwitch={false}></MyNavbar>
+            <div className="loading" style={{textAlign: "center",top: "30vh",height: "50vh",color: "white",display : (!isLoading ? 'none' : 'block' )}}>
+				<i className="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+			</div>
             <div className={"App Assist "+theme}>
                 Assist
             </div>
