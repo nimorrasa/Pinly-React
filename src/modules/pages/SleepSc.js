06746c595374841223412f7cb235ee6f266e665e
nimorrasa <orrasa.nim@gmail.com>
2020-04-26 02:02:44 +0700

    add comment

--- src/modules/pages/SleepSc.js
+++ src/modules/pages/SleepSc.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from 'reactstrap';
 import '../css/Sleep_score.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import Component and Function
+//import Component ที่ต้องใช้
 import PieChart from '../components/graph/PieChart.js';
 import Bar from '../components/SleepScorePage/Bar';
 import SleepScore from '../components/SleepScorePage/SleepScore';  //show content SleepScore
@@ -20,90 +20,97 @@ import { useCookies } from 'react-cookie';
 import { Route, Switch,BrowserRouter, Link } from 'react-router-dom';
 import { get_mic_summary, getDetail, get_today_string, toPercent, get_date_string } from '../helpers';
 import LoadingScreen from 'react-loading-screen';
-//Create Component - JSX 
+
+//สร้าง component ชื่อ SleepSc โดยให้รับตัวแปรมาเป็น props ด้วย (React Hook)
 const SleepSc = (props) => {
-  const history = useHistory();
-  const [cookies, setCookie, removeCookie] = useCookies(['theme']);
 
-  const [sleepScoreToday,setSleepScoreToday] = useState(0);
-  const [sleepScoreYesterday,setSleepScoreYesterday] = useState(0);
-  const [temp,setTemp] = useState(0);
-  const [summaryMics,setSummaryMics] = useState({
-	  hours : [],
-	  series : []
-  });
-  const [hearthRate,setHearthRate] = useState(0);
-  const [ userData, setUserData ] = useState({});
-  const [ isLoading, setIsLoading ] = useState(true);
-  const [theme,setTheme] = useState(props.theme);
-  const [navbarTheme, setNavbarTheme] = useState(props.theme === 'theme_dark' ? 'dark' : 'light');
-  const [isHoverGoToSleep,setIsHoverGoToSleep] = useState(false);
+	//ประกาศตัวแปรที่จำเป็นต้องใช้ใน component นี้
+	const history = useHistory();
+	const [cookies, setCookie, removeCookie] = useCookies(['theme']);
 
-  const [currentSleep,setCurrentSleep] = useState(new Date());
-  const [currentWakeUp,setCurrentWakeUp] = useState(new Date());
-  const [totalSleep,setTotalSleep] = useState(0);
+	const [sleepScoreToday,setSleepScoreToday] = useState(0);
+	const [sleepScoreYesterday,setSleepScoreYesterday] = useState(0);
+	const [temp,setTemp] = useState(0);
+	const [summaryMics,setSummaryMics] = useState({
+		hours : [],
+		series : []
+	});
+	const [hearthRate,setHearthRate] = useState(0);
+	const [ userData, setUserData ] = useState({});
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [theme,setTheme] = useState(props.theme);
+	const [navbarTheme, setNavbarTheme] = useState(props.theme === 'theme_dark' ? 'dark' : 'light');
+	const [isHoverGoToSleep,setIsHoverGoToSleep] = useState(false);
 
-  const handleNavbarThemeChange = useCallback((current_theme) => {
-    setNavbarTheme(current_theme);
-    setTheme('theme_'+current_theme);
-    props.onChangeTheme('theme_'+current_theme);
-  },[setNavbarTheme,setTheme]);
+	const [currentSleep,setCurrentSleep] = useState(new Date());
+	const [currentWakeUp,setCurrentWakeUp] = useState(new Date());
+	const [totalSleep,setTotalSleep] = useState(0);
+
+	// สร้าง callback function สำหรับการเปลี่ยนแปลง theme
+	const handleNavbarThemeChange = useCallback((current_theme) => {
+		setNavbarTheme(current_theme);
+		setTheme('theme_'+current_theme);
+		props.onChangeTheme('theme_'+current_theme);
+	},[setNavbarTheme,setTheme]);
 
 
-  useEffect(() => {
-      handleNavbarThemeChange(cookies.theme);
-  },[cookies.theme]);
+	// update theme ด้วย cookie
+	useEffect(() => {
+		handleNavbarThemeChange(cookies.theme);
+	},[cookies.theme]);
 
-  const goToHistory = useCallback(() => { history.push('/history'); },[]);
+	const goToHistory = useCallback(() => { history.push('/history'); },[]);
 
-  useEffect(() => {
-    handleNavbarThemeChange(cookies.theme);
-		async function fetchData (user_id) {
-            let user = await firebase.database().ref('/users/' + user_id).once('value');
-			return user.val();
-		}
+	// สำหรับตั้งค่าตัวแปรต่างๆก่อนจะ reder html ออกมา โดยจะดึงข้อมูลจาก firebase และ หลังบ้านมาอัพเดทเพื่อนำไปแสดง
+	useEffect(() => {
+		handleNavbarThemeChange(cookies.theme);
+			async function fetchData (user_id) {
+				let user = await firebase.database().ref('/users/' + user_id).once('value');
+				return user.val();
+			}
 
-		async function fetchDataHardware (date,mac_address) {
-			let hardwares = await getDetail(date,mac_address);
-			return !hardwares.doc ? null : hardwares.doc;
-		}
+			async function fetchDataHardware (date,mac_address) {
+				let hardwares = await getDetail(date,mac_address);
+				return !hardwares.doc ? null : hardwares.doc;
+			}
+			
+			async function fetchDataMic (mac_address) {
+				let date = get_today_string();
+				let mics = await get_mic_summary(date,mac_address);
+				return !mics.doc ? null : formatMicData(mics.doc);
+			}
 		
-		async function fetchDataMic (mac_address) {
-			let date = get_today_string();
-			let mics = await get_mic_summary(date,mac_address);
-			return !mics.doc ? null : formatMicData(mics.doc);
-		}
-    
-        firebase.auth().onAuthStateChanged(async function(user) {
-			if (user) {
-				let data = await fetchData(user.uid);
-				setUserData(data);
+			firebase.auth().onAuthStateChanged(async function(user) {
+				if (user) {
+					let data = await fetchData(user.uid);
+					setUserData(data);
 
-				let today_data = await fetchDataHardware(get_today_string(),data && data.mac_address);
-				if(today_data && today_data.Sleep_Score_Today) setSleepScoreToday(toPercent(today_data.Sleep_Score_Today));
-                setTotalSleep(data && data.sleep_period);
-                setCurrentSleep(data && data.current_sleep);
-				setCurrentWakeUp(data && data.current_wakeup);
-				if(today_data && today_data.Heart_Rate) setHearthRate(today_data.Heart_Rate);
-                if(today_data && today_data.Temp)  setTemp(today_data.Temp);
+					let today_data = await fetchDataHardware(get_today_string(),data && data.mac_address);
+					if(today_data && today_data.Sleep_Score_Today) setSleepScoreToday(toPercent(today_data.Sleep_Score_Today));
+					setTotalSleep(data && data.sleep_period);
+					setCurrentSleep(data && data.current_sleep);
+					setCurrentWakeUp(data && data.current_wakeup);
+					if(today_data && today_data.Heart_Rate) setHearthRate(today_data.Heart_Rate);
+					if(today_data && today_data.Temp)  setTemp(today_data.Temp);
 
-				let date = new Date();
-				date.setDate(date.getDate() - 1);
-				let yesterday_data = await fetchDataHardware(get_date_string(date),data && data.mac_address);
-				if(yesterday_data && yesterday_data) setSleepScoreYesterday(toPercent(yesterday_data.Sleep_Score_Today));
+					let date = new Date();
+					date.setDate(date.getDate() - 1);
+					let yesterday_data = await fetchDataHardware(get_date_string(date),data && data.mac_address);
+					if(yesterday_data && yesterday_data) setSleepScoreYesterday(toPercent(yesterday_data.Sleep_Score_Today));
 
-				let micData = await fetchDataMic(data && data.mac_address);
-				setSummaryMics(micData);
+					let micData = await fetchDataMic(data && data.mac_address);
+					setSummaryMics(micData);
 
-            }else{
-                history.push('/login');
-            }
-            setIsLoading(false);
-        });
-    
+				}else{
+					history.push('/login');
+				}
+				setIsLoading(false);
+			});
+		
 
 	},[]);
 
+	// จัดฟอเมตค่า Mic
 	function formatMicData (array) {
 		let hours = [];
 		let series = [];
@@ -118,9 +125,11 @@ const SleepSc = (props) => {
 		};
 	}
 
+	// สำหรับทำ Hover ให้กับปุ่ม Go to sleep
 	const onMouseOverGoToSleep = useCallback(() => { setIsHoverGoToSleep(true); },[setIsHoverGoToSleep]);
     const onMouseOutGoToSleep = useCallback(() => { setIsHoverGoToSleep(false); },[setIsHoverGoToSleep]);
 
+	// Render ออกไปเป็น html
     return(
 		<LoadingScreen
 			bgColor={theme == 'light' ? '#F5F9FD' : '141313'}
@@ -172,5 +181,5 @@ const SleepSc = (props) => {
   }
 
 
-
+// export เพื่อให้สามารถเรียกใช้ component นี้ได้ในที่อื่นโดยการ import
 export default SleepSc;
